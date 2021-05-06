b2b0bda05b5e6598b993589eb12d9477555195f0
Ray <72799965+BombaDuck@users.noreply.github.com>
2021-05-06 17:55:54 +0800

    Create Data.cs

---
+++ c#/Data.cs
@@ -0,0 +1,43 @@
+namespace SerialTest02
+{
+    internal class RssiData
+    {
+        public string Rssi { get; set; }
+    }
+
+    internal class TemperatureData
+    {
+        public string Temperature { get; set; }
+    }
+    internal class EpcIDData
+    {
+        public string EpcID { get; set; }
+    }
+
+    public class myTagsData
+    {
+
+        public myTagsData(string ID, int Temperature, double xlocation, double ylocation)
+        {
+            this.ID = ID;
+            this.Temperature = Temperature;
+            this.xlocation = xlocation;
+            this.ylocation = ylocation;
+
+        }
+        public myTagsData(string ID)
+        {
+            this.ID = ID;
+
+
+        }
+        public string ID { get; set; }
+
+        public int Temperature { get; set; }
+
+        public double xlocation { get; set; }
+
+        public double ylocation { get; set; }
+
+    }
+}
